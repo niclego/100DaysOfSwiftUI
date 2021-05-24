f88a4eeb41a7ff6ea7ab91674ceb0da0c9979dd4
Nicolas Le Gorrec <nick.p.legorrec@gmail.com>
2021-05-24 16:28:24 -0400

    project 8

---
+++ SwiftUI/Day 39 - 42 | Project 8 | Moonshot/Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Nicolas Le Gorrec on 5/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    var body: some View {
+        NavigationView {
+            List(missions) { mission in
+                NavigationLink(destination: MissionView(mission: mission, astronauts: astronauts)) {
+                    Image(mission.image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 44, height: 44)
+                    
+                    VStack(alignment: .leading) {
+                        Text(mission.displayName)
+                            .font(.headline)
+                        Text(mission.formattedDate)
+                    }
+                }
+            }
+            .navigationBarTitle("Moonshot")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI/Day 39 - 42 | Project 8 | Moonshot/Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Nicolas Le Gorrec on 5/20/21.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
